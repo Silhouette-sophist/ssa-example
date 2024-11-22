a7cb25060954379e6ae6db368bfd4bcca256dbf6
Silhouette <54932288+Silhouette-sophist@users.noreply.github.com>
2024-11-22 22:44:55 +0800

    [Optimize]补充说明

--- visitor/func_visitor.go
+++ visitor/func_visitor.go
@@ -9,6 +9,7 @@ type FuncUsedScanner struct {
 // Visit ast遍历，只获取被导出的标识符
 func (fus *FuncUsedScanner) Visit(node ast.Node) ast.Visitor {
 	switch n := node.(type) {
+	// TODO 只识别导出的标识符[函数、变量、类型]
 	case *ast.Ident:
 		if n.IsExported() {
 			fus.IdentMap[n.Name] = struct{}{}
